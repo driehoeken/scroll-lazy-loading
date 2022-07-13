70372c0310b0cc9c1ee5231ac37e0c627b387237
driehoeken <a.bednarski1203@gmail.com>
2022-07-13 20:19:10 +0200

    data fix v3

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Post = () => {
+    const getData = async () => {
+        fetch("https://raw.githubusercontent.com/driehoeken/scroll-lazy-loading/master/src/data.js")
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch(console.error);
+    };
+    getData();
     return <div className="post"></div>;
 };
 
