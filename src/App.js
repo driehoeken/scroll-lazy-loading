0674c68d588aff1079333682c2408b892081fbf7
driehoeken <a.bednarski1203@gmail.com>
2022-07-14 20:31:01 +0200

    loading new post func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import Post from "./Post/Post";
 
 function App() {
+    const [posts, setPosts] = useState([<Post key={0} />]);
+    const loadNewPost = () => {
+        posts.push(<Post key={posts.length} />);
+    };
+
+    loadNewPost();
+
     return (
         <div className="app">
-            <Post />
+            {posts.map((post) => {
+                return post;
+            })}
         </div>
     );
 }
